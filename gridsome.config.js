32567335580df8e76bbecbfa69b153e070152cae
Andy Stevens <taylorstevens@gmail.com>
2021-09-18 16:09:18 -0400

    Remove erroring and unnecessary dependencies
    - postcss-purgecss
    - postcss-import
    - postcss-preset-env

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,13 +5,10 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 const tailwind = require('tailwindcss')
-const purgecss = require('@fullhuman/postcss-purgecss')
 
 const postcssPlugins = [
   tailwind(),
 ]
-
-if (process.env.NODE_ENV === 'production') postcssPlugins.push(purgecss(require('./purgecss.config.js')))
 
 module.exports = {
     siteName: 'Andy Stevens',
