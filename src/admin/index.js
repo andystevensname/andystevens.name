0c33843a4151a6707ae68bfdd054fcb58fd9ef02
Andy Stevens <taylorstevens@gmail.com>
2021-09-19 18:14:18 -0400

    Finally fix NetlifyCMS styles

--- src/admin/index.js
+++ src/admin/index.js
@@ -1,1 +1,18 @@
 import CMS from "netlify-cms"
+CMS.init()
+CMS.registerPreviewStyle("/netlifycms.css");
+
+var PostPreview = createClass({
+    render: function() {
+      const { entry, getAsset, widgetsFor } = this.props
+  
+      return h('article', {},
+                h('h1', {}, entry.getIn(['data', 'title']) ),
+                h('div', {}, this.props.widgetFor('body'),
+                h('div', { className: 'date'}, entry.getIn(['data', 'date']) ),
+              )
+        )    
+    }
+  })
+
+  CMS.registerPreviewTemplate('post', PostPreview)
