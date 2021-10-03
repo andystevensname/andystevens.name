9780b2ea5cef5c988f37993339cc4ed31b991a12
Andy Stevens <taylorstevens@gmail.com>
2021-10-03 19:45:43 -0400

    Menu navigation decoupling (#21)
    * First-pass at splitting header/navigation
    
    * Decouple Nav and Header
    
    * Migrate Header animation to transform/translate

---
+++ src/components/Navigation.vue
@@ -0,0 +1,38 @@
+<template>
+    <nav id="nav" class="h-full w-60 -translate-x-60 fixed top-0 bottom-0 z-20 overflow-x-hidden transform duration-500">
+        <div class="navContainer h-full border-r border-solid border-white">
+            <font-awesome :icon="['fa', 'times']" v-on:click="$emit('close-nav')" size="lg" class="cursor-pointer text-white"  style="margin: 14px 16px 10px 16px"/>
+            <ul class="border-box w-full list-none px-4 divide-y divide-white m-0">
+                <li class="p-0 m-0 w-full h-12 border-box"><g-link to="/" class="w-full block no-underline text-current">Home</g-link></li>
+                <li class="p-0 m-0 w-full h-12 border-box"><g-link to="/blog/" class="w-full block no-underline text-current">Blog</g-link></li>
+                <li class="p-0 m-0 w-full h-12 border-box"><g-link to="/writing/" class="w-full block no-underline text-current">Writing</g-link></li>
+                <li class="p-0 m-0 w-full h-12 border-box"><g-link to="/about/" class="w-full block no-underline text-current">About</g-link></li>
+            </ul>
+        </div>
+    </nav>
+</template>
+
+<script>
+    export default {
+        name: 'Navigation',
+    }
+</script>
+
+
+<style>
+#nav {
+    display: block;
+}
+
+#nav .navContainer {
+    background-color: #212121;
+}
+
+#nav ul li a {
+    border-bottom: 0;
+}
+
+#nav ul li a:active, #nav ul li a:hover, #nav ul li a:visited {
+    color: white;
+}
+</style>
--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,32 +1,51 @@
 <template>
-  <div id="container w-full h-full relative">
-    <ClientOnly>
-      <Header />
-    </ClientOnly>
-    <main class="w-full flex-grow relative">
-      <slot>
-      </slot>
-    </main>
-    <footer id="footer" class="w-full relative border-t border-white h-12">
-      &copy; 2021 Andy Stevens
-    </footer>
-  </div>
+    <div id="container w-full h-full relative">
+        <Header />
+        <Navigation v-on:close-nav="closeNav" v-hammer:swipe.left="closeNav" />
+        <main class="w-full flex-grow relative">
+            <slot>
+            </slot>
+        </main>
+        <footer id="footer" class="w-full relative border-t border-white h-12">
+            &copy; 2021 Andy Stevens
+        </footer>
+    </div>
 </template>
 
 <static-query>
 query {
-  metadata {
-    siteName
-  }
+    metadata {
+        siteName
+    }
 }
 </static-query>
 
 <script>
 import Header from '~/components/Header.vue'
+import Navigation from '~/components/Navigation.vue'
 
 export default {
-  components: {
-    Header
-  }
+    components: {
+        Header,
+        Navigation
+    },
+    methods: {
+        openNav: function () {
+            document.getElementById("nav").classList.remove('-translate-x-60')
+            /*document.getElementById("nav").style.left = "0";
+            document.getElementById("nav").style.boxShadow = "0px 0px 1.5rem black";*/
+        },
+        closeNav: function() {
+            document.getElementById("nav").classList.add('-translate-x-60');
+            /*document.getElementById("nav").style.left = "-15rem";
+            document.getElementById("nav").style.boxShadow = "unset";*/
+        }
+    },
+    mounted() {
+        self = this;
+        this.$root.$on('open-nav', function() {
+            self.openNav();
+        });
+    }
 }
 </script>
