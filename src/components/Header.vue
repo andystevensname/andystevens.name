cb58e145e2a2af4c9da80971e3ba29fe6444220f
Andy Stevens <taylorstevens@gmail.com>
2021-10-07 22:40:47 -0400

    Clean up icons displaying/not displaying
    Should fix a problem with both day/night icons showing at the same time.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,12 @@
 <template>
     <header id="header" class="block fixed inset-0 border-box z-10 h-12 border-b border-solid border-googleGray bg-white duration-500 transform dark:bg-googleGray dark:border-white bg-opacity-80">
         <font-awesome :icon="['fa', 'bars']" v-on:click="$root.$emit('open-nav')" size="lg" class="cursor-pointer text-googleGray dark:text-white" style="margin: 14px 16px 14px 16px"/>
-        <font-awesome :icon="['fa', 'moon']" v-on:click="changeTheme" size="lg" id="theme-moon" class="cursor-pointer text-googleGray dark:text-white" style="margin: 14px 16px 14px 16px"/>
-        <font-awesome :icon="['fa', 'sun']" v-on:click="changeTheme" size="lg" id="theme-sun" class="cursor-pointer text-googleGray dark:text-white" style="margin: 14px 16px 14px 16px"/>
+        <span v-if="isDark === true">
+            <font-awesome :icon="['fa', 'moon']" v-on:click="changeTheme" size="lg" id="theme-moon" class="cursor-pointer text-googleGray dark:text-white" style="margin: 14px 16px 14px 16px"/>
+        </span>
+        <span v-if="isDark === false">
+            <font-awesome :icon="['fa', 'sun']" v-on:click="changeTheme" size="lg" id="theme-sun" class="cursor-pointer text-googleGray dark:text-white" style="margin: 14px 16px 14px 16px"/>
+        </span>
         <span class="title-text h-12 block float-right mr-4 font-bold">Andy Stevens</span>
     </header>
 </template>
@@ -10,27 +14,32 @@
 <script>
 export default {
     name: 'Header',
+    data: function () {
+        return {
+            isDark: false
+        }
+    },
     methods: {
         setDark: function() {
-            // Whenever the user explicitly chooses dark mode
             localStorage.theme = 'dark'
             document.documentElement.classList.add('dark');
             this.$root.$emit('changeTheme');
-            document.getElementById('theme-moon').classList.add('hidden');
-            document.getElementById('theme-sun').classList.remove('hidden');
-            document.getElementById('theme-moon').style.setProperty('display', 'none');
-            document.getElementById('theme-sun').style.setProperty('display', 'inline-block');
+            this.isDark = true;
+            //document.getElementById('theme-moon').classList.add('hidden');
+            //document.getElementById('theme-sun').classList.remove('hidden');
+            //document.getElementById('theme-moon').style.setProperty('display', 'none');
+            //document.getElementById('theme-sun').style.setProperty('display', 'inline-block');
         },
 
         setLight: function() {
-            // Whenever the user explicitly chooses light mode
             localStorage.theme = 'light'
             document.documentElement.classList.remove('dark');
             this.$root.$emit('changeTheme');
-            document.getElementById('theme-moon').classList.remove('hidden');
-            document.getElementById('theme-sun').classList.add('hidden');
-            document.getElementById('theme-moon').style.setProperty('display', 'inline-block');
-            document.getElementById('theme-sun').style.setProperty('display', 'none');
+            this.isDark = false;
+            //document.getElementById('theme-moon').classList.remove('hidden');
+            //document.getElementById('theme-sun').classList.add('hidden');
+            //document.getElementById('theme-moon').style.setProperty('display', 'inline-block');
+            //document.getElementById('theme-sun').style.setProperty('display', 'none');
         },
 
         // On page load or when changing themes, best to add inline in `head` to avoid FOUC
