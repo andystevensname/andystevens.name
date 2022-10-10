5c363f33caba3aa246469bbf5a1ab60a442f1d1f
Andy Stevens <taylorstevens@gmail.com>
2022-10-10 16:21:38 -0400

    Change ssr to true (#47)
    * Change ssr to true in nuxt.config.js
    
    * recommit bucket correction
    
    * fix bucket name

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ import { defineNuxtConfig } from 'nuxt/config'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  ssr: false,
+  ssr: true,
   modules: [
     '@nuxt/content',
     '@nuxtjs/tailwindcss'
