a18c8a3ea5ab447cf965c67610b7330a6851bf9b
Andy Stevens <taylorstevens@gmail.com>
2022-10-13 18:54:47 -0400

    Improve SSG Generation (#49)
    * change [...slug].vue to [slug].vue
    
    * rename catchall [[slug]].vue
    
    * improve static site generation by declaring some routes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,12 +2,17 @@ import { defineNuxtConfig } from 'nuxt/config'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  ssr: true,
   modules: [
     '@nuxt/content',
     '@nuxtjs/tailwindcss'
   ],
   css: [
     '@fortawesome/fontawesome-svg-core/styles.css'
-  ]
+  ],
+  nitro: {
+    prerender: {
+      crawlLinks: true,
+      routes: ['/about', '/writing'],
+    },
+  },
 })
